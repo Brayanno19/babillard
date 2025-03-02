ce0699dd811725f48425ac5195090cb28ac0acc4
Brayanno19 <teupebrayanno@gmail.com>
2025-03-03 00:23:38 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Babillard</title>
+    <link href="./Style/indexStyle.css">
+</head>
+<body>
+    <h1>Mon Babillard</h1>
+</body>
+</html>
